9a78b19a0fd6b45ac51eb2c996c94d6f8688ee34
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-28 22:06:36 +0530

    ADDED: Functionality to edit existing contact

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -10,17 +10,27 @@ public class AddressBookMain {
 
 		System.out.println("Welcome to Address Book Program");
 		int choice;
-		System.out.println("1.Add Contact \n2.Exit");
-		choice = sc.nextInt();
-		sc.nextLine();
-		switch (choice) {
-		case 1:
-			addressBook.addContact();
-			break;
-		default:
-			System.out.println("Choose correct option from above mentioned option only!!");
-			break;
-		}
+		do {
+			System.out.println("1.Add Contact \n2.View Contact \n3.Edit Contact \n4.Exit");
+			choice = sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.viewContact();
+				break;
+			case 3:
+				addressBook.editContact();
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("Choose correct option from above mentioned option only!!");
+				break;
+			}
+		} while (choice != 4);
 		sc.close();
 	}
 }
--- src/com/bridgelabz/addressbook/AddressBookService.java
+++ src/com/bridgelabz/addressbook/AddressBookService.java
@@ -2,11 +2,11 @@ package com.bridgelabz.addressbook;
 
 import java.util.Scanner;
 
-
 public class AddressBookService {
 //	public void create
 //	AddressBook firstBook = new AddressBook(null)
-	
+	static AddressBook firstBook;
+
 	public void addContact() {
 		Scanner sc = AddressBookMain.sc;
 		System.out.println("Enter First Name: ");
@@ -28,7 +28,80 @@ public class AddressBookService {
 		String email = sc.nextLine();
 
 		Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNum, email);
-		AddressBook firstBook = new AddressBook(contact);
-		System.out.println("Contact Details are added!\n"+firstBook.person);
+		firstBook = new AddressBook(contact);
+		System.out.println("Contact Details are added!\n" + firstBook.person);
+	}
+
+	public void editContact() {
+		if( firstBook==null) {
+			System.out.println("Address Book is empty!");
+			return;
+		}
+		Scanner sc = AddressBookMain.sc;
+		System.out.println("Enter first name of contact that you want to edit: ");
+		String firstName = sc.nextLine();
+		if (firstBook.person.getFirstName().equals(firstName)) {
+			System.out.println("Index: " + firstBook.person);
+			System.out.println("Enter your choice: \n1.First Name \n2.Last Name \n3.Address \n4.City \n5.State "
+					+ "\n6.Zip Code \n7.Phone Number \n8.EmailId \n9.Skip Editing");
+			int choice = sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter new First Name: ");
+				String newFirstName = sc.nextLine();
+				firstBook.person.setFirstName(newFirstName);
+				break;
+			case 2:
+				System.out.println("Enter new Last Name: ");
+				String newLastName = sc.nextLine();
+				firstBook.person.setLastName(newLastName);
+				break;
+			case 3:
+				System.out.println("Enter new Address: ");
+				String newAddress = sc.nextLine();
+				firstBook.person.setAddress(newAddress);
+				break;
+			case 4:
+				System.out.println("Enter new City: ");
+				String newCity = sc.nextLine();
+				firstBook.person.setCity(newCity);
+				break;
+			case 5:
+				System.out.println("Enter new State: ");
+				String newState = sc.nextLine();
+				firstBook.person.setState(newState);
+				break;
+			case 6:
+				System.out.println("Enter new ZipCode: ");
+				int newZip = sc.nextInt();
+				firstBook.person.setZip(newZip);
+				break;
+			case 7:
+				System.out.println("Enter new Phone Number: ");
+				long newPhoneNum = sc.nextLong();
+				firstBook.person.setPhoneNumber(newPhoneNum);
+				break;
+			case 8:
+				System.out.println("Enter new Email Id: ");
+				String newEmail = sc.nextLine();
+				firstBook.person.setEmail(newEmail);
+				break;
+			case 9:
+				break;
+			default:
+				System.out.println("Choose right choice!!");
+			}
+		} else {
+			System.out.println("No contact with such phone number exists!!");
+		}
+	}
+
+	public void viewContact() {
+		if( firstBook==null) {
+			System.out.println("Address Book is empty!");
+			return;
+		}
+		System.out.println(firstBook.person);
 	}
 }
