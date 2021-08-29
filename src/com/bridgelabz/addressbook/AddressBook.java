1c3855855dc41c98b48bc00057801fe9517a8b4c
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-30 01:40:07 +0530

    ADDED: Functionality to access multiple address books

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.addressbook;
 
 public class AddressBook {
+	String name;
 	Contact[] persons;
 
-	public AddressBook(Contact[] persons) {
+	public AddressBook(String addressBookName, Contact[] persons) {
 		super();
+		this.name = addressBookName;
 		this.persons = persons;
 	}
 
--- src/com/bridgelabz/addressbook/AddressBookImpl.java
+++ src/com/bridgelabz/addressbook/AddressBookImpl.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class AddressBookImpl implements AddressBookIF {
 
-	Contact[] arrayOfContacts;
-	AddressBook firstBook;
+	AddressBook addressBook;
 	int index;
 	int numberOfElements;
 	static final Scanner sc = AddressBookMain.sc;
 
-
-	public AddressBookImpl(int maximumContacts) {
+	public AddressBookImpl(String addressBookName, int maximumContacts) {
 
 		super();
-		this.arrayOfContacts = new Contact[maximumContacts];
-		this.firstBook = new AddressBook(arrayOfContacts);
+		this.addressBook = new AddressBook(addressBookName, new Contact[maximumContacts]);
 		this.index = -1;
 		numberOfElements = 0;
 	}
 
 	@Override
+	public String getAddressBookName() {
+		return addressBook.name;
+	}
+
+	@Override
 	public void addContact() {
 
-		if (numberOfElements == arrayOfContacts.length) {
+		if (numberOfElements == addressBook.persons.length) {
 			System.out.println("Maximum contacts taken, no space left!");
 			return;
 		}
@@ -46,34 +48,34 @@ public class AddressBookImpl implements AddressBookIF {
 		System.out.println("Enter your email Id: ");
 		String email = sc.nextLine();
 
-		for (int i = 0; i < arrayOfContacts.length; i++) {
+		for (int i = 0; i < addressBook.persons.length; i++) {
 
-			index = (++index) % arrayOfContacts.length;
+			index = (++index) % addressBook.persons.length;
 
-			if (firstBook.persons[index] == null) {
+			if (addressBook.persons[index] == null) {
 				break;
 			}
 		}
-		arrayOfContacts[index] = new Contact(firstName, lastName, address, city, state, zip, phoneNum, email);
+		addressBook.persons[index] = new Contact(firstName, lastName, address, city, state, zip, phoneNum, email);
 		numberOfElements++;
-		System.out.println("Contact Details are added!\n" + firstBook.persons[index]);
+		System.out.println("Contact Details are added!\n" + addressBook.persons[index]);
 	}
 
 	@Override
 	public void editContact() {
 
-		
 		boolean found = false;
 		System.out.println("Enter first name of contact that you want to edit: ");
 		String firstName = sc.nextLine();
 		System.out.println("Enter last name of contact that you want to edit: ");
 		String lastName = sc.nextLine();
-		for (int i = 0; i < arrayOfContacts.length; i++) {
+		for (int i = 0; i < addressBook.persons.length; i++) {
 
-			if (firstBook.persons[i] != null && firstBook.persons[i].getFirstName().equals(firstName)
-					&& firstBook.persons[i].getLastName().equals(lastName)) {
+			if (addressBook.persons[i] != null && addressBook.persons[i].getFirstName().equals(firstName)
+					&& addressBook.persons[i].getLastName().equals(lastName)) {
+				
 				found = true;
-				System.out.println("Index: " + firstBook.persons[i]);
+				System.out.println("Index: " + addressBook.persons[i]);
 				System.out.println("Enter your choice: \n1.First Name \n2.Last Name \n3.Address \n4.City \n5.State "
 						+ "\n6.Zip Code \n7.Phone Number \n8.EmailId \n9.Skip Editing");
 				int choice = sc.nextInt();
@@ -82,42 +84,42 @@ public class AddressBookImpl implements AddressBookIF {
 				case 1:
 					System.out.println("Enter new First Name: ");
 					String newFirstName = sc.nextLine();
-					firstBook.persons[i].setFirstName(newFirstName);
+					addressBook.persons[i].setFirstName(newFirstName);
 					break;
 				case 2:
 					System.out.println("Enter new Last Name: ");
 					String newLastName = sc.nextLine();
-					firstBook.persons[i].setLastName(newLastName);
+					addressBook.persons[i].setLastName(newLastName);
 					break;
 				case 3:
 					System.out.println("Enter new Address: ");
 					String newAddress = sc.nextLine();
-					firstBook.persons[i].setAddress(newAddress);
+					addressBook.persons[i].setAddress(newAddress);
 					break;
 				case 4:
 					System.out.println("Enter new City: ");
 					String newCity = sc.nextLine();
-					firstBook.persons[i].setCity(newCity);
+					addressBook.persons[i].setCity(newCity);
 					break;
 				case 5:
 					System.out.println("Enter new State: ");
 					String newState = sc.nextLine();
-					firstBook.persons[i].setState(newState);
+					addressBook.persons[i].setState(newState);
 					break;
 				case 6:
 					System.out.println("Enter new ZipCode: ");
 					int newZip = sc.nextInt();
-					firstBook.persons[i].setZip(newZip);
+					addressBook.persons[i].setZip(newZip);
 					break;
 				case 7:
 					System.out.println("Enter new Phone Number: ");
 					long newPhoneNum = sc.nextLong();
-					firstBook.persons[i].setPhoneNumber(newPhoneNum);
+					addressBook.persons[i].setPhoneNumber(newPhoneNum);
 					break;
 				case 8:
 					System.out.println("Enter new Email Id: ");
 					String newEmail = sc.nextLine();
-					firstBook.persons[i].setEmail(newEmail);
+					addressBook.persons[i].setEmail(newEmail);
 					break;
 				case 9:
 					break;
@@ -127,6 +129,7 @@ public class AddressBookImpl implements AddressBookIF {
 			}
 		}
 		if (!found) {
+			
 			System.out.println("No contact with such name exists!!");
 		}
 	}
@@ -134,10 +137,11 @@ public class AddressBookImpl implements AddressBookIF {
 	@Override
 	public void viewContact() {
 
-		for (int i = 0; i < arrayOfContacts.length; i++) {
+		for (int i = 0; i < addressBook.persons.length; i++) {
 
-			if (firstBook.persons[i] != null) {
-				System.out.println(firstBook.persons[i]);
+			if (addressBook.persons[i] != null) {
+				
+				System.out.println(addressBook.persons[i]);
 			}
 		}
 	}
@@ -145,20 +149,21 @@ public class AddressBookImpl implements AddressBookIF {
 	@Override
 	public void deleteContact() {
 
-		
 		boolean found = false;
 		System.out.println("Enter first name of contact that you want to delete: ");
 		String firstName = sc.nextLine();
 		System.out.println("Enter last name of contact that you want to delete: ");
 		String lastName = sc.nextLine();
-		for (int i = 0; i < arrayOfContacts.length; i++) {
+		for (int i = 0; i < addressBook.persons.length; i++) {
 
-			if (firstBook.persons[i] != null && firstBook.persons[i].getFirstName().equals(firstName)
-					&& firstBook.persons[i].getLastName().equals(lastName)) {
+			if (addressBook.persons[i] != null && addressBook.persons[i].getFirstName().equals(firstName)
+					&& addressBook.persons[i].getLastName().equals(lastName)) {
+				
 				found = true;
-				System.out.println("Index: " + firstBook.persons[i] + "\nDelete (y/n)");
+				System.out.println("Index: " + addressBook.persons[i] + "\nDelete (y/n)");
 				if ((sc.nextLine()).equals("y")) {
-					firstBook.persons[i] = null;
+					
+					addressBook.persons[i] = null;
 					numberOfElements--;
 					System.out.println("Contact is deleted!");
 				}
@@ -182,5 +187,50 @@ public class AddressBookImpl implements AddressBookIF {
 
 			return false;
 		}
+	}
+
+	public void openAddressbook() {
+		int choice;
+		do {
+			System.out.println("1.Add Contact \n2.View Contact \n3.Edit Contact \n4.Delete Contact \n5.Exit from the AddressBook");
+			choice = sc.nextInt();
+			sc.nextLine();
+			
+			switch (choice) {
+			
+			case 1:
+				this.addContact();
+				break;
+				
+			case 2:
+				if (!this.isAddressBookEmpty()) {
+
+					this.viewContact();
+				}
+				break;
+				
+			case 3:
+				if (!this.isAddressBookEmpty()) {
+
+					this.editContact();
+				}
+				break;
+				
+			case 4:
+				if (!this.isAddressBookEmpty()) {
+
+					this.deleteContact();
+				}
+				break;
+				
+			case 5:
+				break;
+				
+			default:
+				System.out.println("Choose correct option from above mentioned option only!!");
+				break;
+			}
+		} while (choice != 5);
+
 	}
 }
